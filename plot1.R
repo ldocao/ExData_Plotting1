9500befea19bbacc04dc83a7430e85a4b72820b0
ldocao <ldocao@gmail.com>
2015-05-15 11:56:48 +0200

    change df shortcut

--- plot1.R
+++ plot1.R
@@ -6,12 +6,12 @@ data$Date <- as.Date(data$Date,format="%d/%m/%Y")
 #subsetting only the interesting dates
 select_date <- as.Date(c("2007-02-01","2007-02-02"),format="%Y-%m-%d")
 subset_data <- data[data$Date %in% select_date,]
-d<-subset_data #shortcut
-d$Global_active_power <- as.numeric(as.character(d$Global_active_power)) #convert factors to numeric
+df<-subset_data #shortcut
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power)) #convert factors to numeric
 
 #make the plot
 png("plot1.png")
-hist(d$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(df$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
 
 
